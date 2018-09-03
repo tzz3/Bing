0945db853070be41683c5a7453b443efa0b4891e
冰阔落 <1789803837@qq.com>
2018-09-04 00:34:32 +0800

    完成投票功能

--- polls/views.py
+++ polls/views.py
@@ -11,7 +11,7 @@ from django.urls import reverse
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
-    context_object_naem = 'latest_question_list'
+    context_object_name = 'latest_question_list'
 
     def get_queryset(self):
         return Question.objects.order_by('-pub_date')[:5]
