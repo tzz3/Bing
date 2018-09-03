549b06fa7a595280cbb574d46b63d2ed7a1de6d5
冰阔落 <1789803837@qq.com>
2018-09-04 01:36:43 +0800

    学习编写polls自动化测试样例

--- polls/views.py
+++ polls/views.py
@@ -5,7 +5,7 @@ from .models import Question, Choice
 from django.template import loader
 from django.http import Http404
 from django.views import generic
-
+from django.utils import timezone
 from django.urls import reverse
 
 
@@ -14,12 +14,23 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        不返回未来发布的投票
+        :return:
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        :return:
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
